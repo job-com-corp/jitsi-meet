5f5c98acfa650b211021ab0eb392929eb8bd2a17
Ugljesa Stosic <ugljesa.stosic@team.ai>
2024-02-09 15:43:29 +0100

    CI tag version remove sha

--- release-jobcom.sh
+++ release-jobcom.sh
@@ -2,8 +2,7 @@
 
 version=jobcom-2.0.9164
 timestamp=$(date +%Y%m%d)
-git_sha=$(git rev-parse --short HEAD)
-git_tag="${version}-${timestamp}-${git_sha}"
+git_tag="${version}-${timestamp}"
 echo "New git tag: $git_tag"
 
 git tag $git_tag
