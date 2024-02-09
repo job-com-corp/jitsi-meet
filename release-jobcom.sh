e5d0edc5c460e38a5569e70937248e9867d6c3aa
Ugljesa Stosic <ugljesa.stosic@team.ai>
2024-02-09 15:14:04 +0100

    CI add timestamp into the tag name

--- release-jobcom.sh
+++ release-jobcom.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-version=2.0.9164
+version=jobcom-2.0.9164
+timestamp=$(date +%Y%m%d%H%M)"
 git_sha=$(git rev-parse --short HEAD)
-git_tag="jobcom-${version}.${git_sha}"
+git_tag="${version}-${timestamp}-${git_sha}"
 echo "New git tag: $git_tag"
 
 git tag $git_tag
