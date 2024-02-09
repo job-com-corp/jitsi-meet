9b6983290050302ad657f1edbf8a5b209d24b51f
Ugljesa Stosic <ugljesa.stosic@team.ai>
2024-02-09 15:28:02 +0100

    CI use branchname as tag version

--- release-jobcom.sh
+++ release-jobcom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-version=jobcom-2.0.9164
-timestamp=$(date +%Y%m%d%H%M)
+version=$(git symbolic-ref --short HEAD)
+timestamp=$(date +%Y%m%d%)
 git_sha=$(git rev-parse --short HEAD)
 git_tag="${version}-${timestamp}-${git_sha}"
 echo "New git tag: $git_tag"
