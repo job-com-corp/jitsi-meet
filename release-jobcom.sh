156533160e591e029d01fec76a29ad1b593375e6
Ugljesa Stosic <ugljesa.stosic@team.ai>
2024-02-26 14:57:33 +0100

    [CI] try and check IS_RELEASE_TAG complete

--- release-jobcom.sh
+++ release-jobcom.sh
@@ -3,12 +3,13 @@
 echo "Run $0 -stable for stable release (without timestamp)"
 arg=$1
 
-# Check release-jitsi-meet-assets.yml workflow for version regex
-version_internal=3.test
+version_internal=3.8.0
 version_jitsi=2.0.9258
-version=jobcom-${version_jitsi}-${version_internal}
-timestamp=$(date +%Y%m%d%H%M)
 
+# Check release-jitsi-meet-assets.yml workflow for version/tag regex
+version=jobcom-${version_jitsi}-${version_internal}
+
+timestamp=$(date +%Y%m%d%H%M)
 if [[ $arg == '-stable' ]]; then
   git_tag="${version}"
   echo "STABLE release!"
