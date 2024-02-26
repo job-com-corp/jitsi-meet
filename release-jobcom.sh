32b324f0ec3a9f65d609544127c6a8fde02a4ab5
Ugljesa Stosic <ugljesa.stosic@team.ai>
2024-02-26 12:10:36 +0100

    CI Release script

--- release-jobcom.sh
+++ release-jobcom.sh
@@ -1,9 +1,25 @@
 #!/bin/bash -e
 
-version=jobcom-2.0.9258
-timestamp=$(date +%Y%m%d)
-git_tag="${version}-${timestamp}"
-echo "New git tag: $git_tag"
+echo "Run $0 -stable for stable release (without timestamp)"
 
-git tag $git_tag
-git push origin $git_tag
+arg=$1
+version_internal=3.8.0
+version_jitsi=2.0.9258
+version=jobcom-${version_jitsi}-${version_internal}
+timestamp=$(date +%Y%m%d%H%M)
+
+if [[ $arg == '-stable' ]]; then
+  git_tag="${version}"
+  echo "STABLE release!"
+else
+  git_tag="${version}-${timestamp}"
+fi
+echo -e "Publishing new git release:\n\n$git_tag\n"
+
+read -p "Are you sure? [y/N]: " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git tag $git_tag
+  git push origin $git_tag
+else
+  echo -e "\nNope"
+fi
